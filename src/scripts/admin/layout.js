a9d0728fb15406d8d5923dbc1c05f90a86684650
idlakshan <id.lakshan21@gmail.com>
2023-11-22 10:55:23 +0530

    When navigation buttons were clicked, it will change colour.

---
+++ src/scripts/admin/layout.js
@@ -0,0 +1,18 @@
+ //Admin navigation Button Clicked
+ let nav = document.querySelector(".aside-nav-button-list"),
+ navList = nav.querySelectorAll("li"),
+
+ totalNav = navList.length;
+
+for (let i = 0; i < totalNav; i++) {
+ const button = navList[i].querySelector("div");
+
+ button.addEventListener('click', function () {
+     for (let j = 0; j < totalNav; j++) {
+         navList[j].querySelector("div").classList.remove("active");
+     }
+
+     this.classList.add("active");
+ })
+
+}
